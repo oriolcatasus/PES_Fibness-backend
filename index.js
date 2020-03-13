e86b397b6161bac53147b9e2168965aa75084e9c
kennyangelfib <36155923+kennyangelfib@users.noreply.github.com>
2020-03-13 01:03:19 +0100

    Using Template literal (´ ´) (grave accent)

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 
-const app = express();
+const app = express()
+const port = 3000
 
 app.get("/test", (req, res) => {
     console.log("Hello");
     res.sendStatus(200);
 });
 
-app.listen(3000, () => {
-    console.log("Server started at http://localhost:${port}");
+app.listen(port, () => 
+{
+    console.log(`Server started at port ${port}`);
+    /*Template literals are enclosed by the backtick (` `)  (grave accent)
+    The next console.log is the same like the first one.The main diference is that the first one uses 
+    template literals (` `) (grave accent) instead of using (" ")(quaotation marks) */
+    //console.log("Server started at port "+port);
 });
