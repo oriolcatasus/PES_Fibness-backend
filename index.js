4e52cc0f208aea815357f05b056922f8bd6bd3b3
oriol.catasus.i <oriol.catasus.i@est.fib.upc.edu>
2020-03-16 20:41:02 +0100

    Use Pool instead of Client for performance reasons

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const dbCtrl = require("./dbCtrl.js");
+const dbCtrl = require("./src/dbCtrl");
 
 const app = express()
 const port = 3000
@@ -13,22 +13,20 @@ app.get("/test", (req, res) => {
 });
 
 app.post("/users", async (req, res) => {
-    console.log(req.body);
+    //console.log(req.body);
     try {
         await dbCtrl.insert("usuarios", req.body);
         res.sendStatus(200);
-    } catch(e) {
-        console.error(e.message);
+    } catch (e) {
+        //console.error(e.message);
         res.status(400).send(e.message);
-    }    
+    }
 });
 
 app.listen(port, () => 
 {
-    //console.log(`Server started at port ${port}`);
     /*Template literals are enclosed by the backtick (` `)  (grave accent)
     The next console.log is the same like the first one.The main diference is that the first one uses 
     template literals (` `) (grave accent) instead of using (" ")(quaotation marks) */
     console.log("Server started at port "+port);
-    dbCtrl.init();
 });
