de09010aeb9aa85522013bb8559f80ca6f7701bb
oriol.catasus.i <oriol.catasus.i@est.fib.upc.edu>
2020-03-19 19:53:32 +0100

    Add first userModel test

--- src/ctrls/dbCtrl.js
+++ src/ctrls/dbCtrl.js
@@ -1,18 +1,37 @@
 const { Pool } = require('pg');
 
+const database = (function() {
+    let env = process.env.PGDATABASE;
+    if (env === undefined) {
+        return "fibness";
+    }
+    return env
+})();
+//console.log(database);
+
 const pool = new Pool({
     user: "fibness",
     host: "10.4.41.146",
-    database: "fibness",
+    database,
     password: "pes08",
     port: 5432
 });
+
+async function select(query) {
+    return await pool.query(query);
+}
 
 async function insert(query) {
     return await pool.query(query);
 }
 
+async function del(query) {
+    return await pool.query(query);
+}
+
 
 module.exports = {
-    insert
+    select,
+    insert,
+    del
 }
---
+++ test/userModelTest.js
@@ -0,0 +1,28 @@
+const assert = require("assert");
+
+const user = require("../src/models/userModel");
+const dbCtrl = require("../src/ctrls/dbCtrl");
+
+describe("userModel script", function() {
+    describe("create function", function() {
+        it("should return user created correctly", async function() {
+            let newUser = {
+                nombre: "Oriol",
+                password: "hash",
+                email: "oriol@example.com",
+            }
+            await user.create(newUser);
+            let query = "SELECT nombre, password, email " +
+                        "FROM usuarios " + 
+                        "WHERE nombre = 'Oriol'";
+            let res = (await dbCtrl.select(query)).rows[0];
+            assert.equal(newUser.nombre, res.nombre);
+            assert.equal(newUser.password, res.password);
+            assert.equal(newUser.email, newUser.email);
+        });
+
+        afterEach(async function() {
+            await dbCtrl.del("DELETE FROM usuarios WHERE nombre = 'Oriol'");
+        })
+    });
+});
