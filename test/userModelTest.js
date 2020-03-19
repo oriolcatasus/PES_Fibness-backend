78b3bdcb1413e3094a73b66bff2a1817bf6d5b8f
oriol.catasus.i <oriol.catasus.i@est.fib.upc.edu>
2020-03-19 20:12:05 +0100

    Add tests for db constraint violation

--- test/userModelTest.js
+++ test/userModelTest.js
@@ -16,9 +16,28 @@ describe("userModel script", function() {
                         "FROM usuarios " + 
                         "WHERE nombre = 'Oriol'";
             let res = (await dbCtrl.select(query)).rows[0];
+            
             assert.equal(newUser.nombre, res.nombre);
             assert.equal(newUser.password, res.password);
             assert.equal(newUser.email, newUser.email);
+        });
+
+        it("should return unique constraint violation", async function() {
+            let newUser = {
+                nombre: "Oriol",
+                password: "hash",
+                email: "oriol@example.com",
+            }
+            await user.create(newUser);
+            assert.rejects(async () => user.create(newUser), Error);
+        });
+
+        it("should return not null constraint violation", async function() {
+            let newUser = {
+                nombre: "Oriol",
+                email: "oriol@example.com",
+            }
+            assert.rejects(async () => user.create(newUser), Error);
         });
 
         afterEach(async function() {
