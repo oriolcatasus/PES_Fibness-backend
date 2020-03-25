2f85c97db3ab1aa3bd1cf15565a23b2b5970c69b
kennyangelfib <36155923+kennyangelfib@users.noreply.github.com>
2020-03-25 18:02:53 +0100

    Codigo de creacion entrenamiento sin tests

--- routes/training.js
+++ routes/training.js
@@ -4,7 +4,34 @@ const training = require("../src/models/trainingModel");
 
 const router = express.Router();
 
-router.delete('/:nombre', async (req, res) => {
+
+//create
+
+router.post('/', async (req, res) => {
+    //console.log(req.body);
+    try {
+        await training.create(req.params.nombre);
+        res.sendStatus(200);
+    } catch (e) {
+        //console.error(e.message);
+        res.status(400).send(e.message);
+    }
+});
+
+//update training
+router.put('/:idElemento', async (req, res) => {
+    //console.log(req.body);
+    try {
+        await training.update(req.params.nombre);
+        res.sendStatus(200);
+    } catch (e) {
+        //console.error(e.message);
+        res.status(400).send(e.message);
+    }
+});
+
+//delete
+router.delete('/:idElemento', async (req, res) => {
     //console.log(req.body);
     try {
         await training.del(req.params.nombre);
@@ -14,6 +41,5 @@ router.delete('/:nombre', async (req, res) => {
         res.status(400).send(e.message);
     }
 });
-
 
 module.exports = router;
--- src/models/trainingModel.js
+++ src/models/trainingModel.js
@@ -1,13 +1,29 @@
 const dbCtrl = require("../ctrls/dbCtrl");
 
-async function del(nombre) {
+async function create(elemento) {
+    let query = {
+        text: "INSERT INTO elementos(nombre,descripcion) VALUES ($1, $2, $3)",
+        values: [elemento.nombre, elemento.descripcion]
+    }
+    await dbCtrl.execute(query);
+}
+
+async function update(elemento) {
+    let query = {
+        text: "UPDATE TABLE FROM elementos WHERE idElemento = $1 SET nombre = $2 ,descripcion = $3",
+        values: [elemento.idElemento,elemento.nombre,elemnto.descripcion]
+    }
+    await dbCtrl.execute(query);
+}
+
+async function del(elemento) {
     let query = {
         text: "DELETE FROM elementos WHERE nombre = $1",
-        values: [nombre]
+        values: [elemento.idElemento]
     }
     await dbCtrl.execute(query);
 }
 
 module.exports = {
-    del
+    create,update,del
 }
