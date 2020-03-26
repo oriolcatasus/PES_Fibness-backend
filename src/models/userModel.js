dfcd29228ba3d4ae5e32a427ed674b82b6281b5b
oriol.catasus.i <oriol.catasus.i@est.fib.upc.edu>
2020-03-27 00:02:58 +0100

    Add new create user tests

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,9 +1,17 @@
 const dbCtrl = require("../ctrls/dbCtrl");
 
 async function create(user) {
-    let query = {
-        text: "INSERT INTO usuarios(nombre, password, email) values($1, $2, $3)",
-        values: [user.nombre, user.password, user.email]
+    if (user.facebook === undefined) {
+        user.facebook = false;
+    }
+    const query = {
+        text: "INSERT INTO usuarios(nombre, password, email, facebook) values($1, $2, $3, $4)",
+        values: [
+            user.nombre,
+            user.password,
+            user.email,
+            user.facebook
+        ]
     }
     await dbCtrl.execute(query);
 }
--- test/userModelTest.js
+++ test/userModelTest.js
@@ -3,55 +3,75 @@ const assert = require("assert");
 const user = require("../src/models/userModel");
 const dbCtrl = require("../src/ctrls/dbCtrl");
 
-describe("userModel script", function() {
-    describe("create function", function() {
+describe("userModel", function() {
+    describe("create", function() {
         beforeEach(async function() {
             await dbCtrl.execute("DELETE FROM usuarios");
         });
 
 
-        it("should return user created correctly", async function() {
-            let newUser = {
-                nombre: "Oriol",
-                password: "hash",
-                email: "oriol@example.com",
+        it("should create a new user", async function() {
+            const fakeUser = {
+                nombre: "FakeName",
+                password: "fakeHash",
+                email: "fake@example.com",
             }
-            await user.create(newUser);
+            await user.create(fakeUser);
 
-            let query = {
+            const query = {
                 text: "SELECT nombre, password, email \
                         FROM usuarios \
-                        WHERE nombre = $1",
-                values: ["Oriol"],
+                        WHERE email = $1",
+                values: [fakeUser.email],
             };
-            let res = (await dbCtrl.execute(query)).rows[0];            
-            assert.equal(newUser.nombre, res.nombre);
-            assert.equal(newUser.password, res.password);
-            assert.equal(newUser.email, newUser.email);
-        });
+            const res = (await dbCtrl.execute(query)).rows[0];            
+            assert.equal(fakeUser.nombre, res.nombre);
+            assert.equal(fakeUser.password, res.password);
+            assert.equal(fakeUser.email, res.email);
 
-        it("should return unique constraint violation", async function() {
-            let newUser = {
-                nombre: "Oriol",
-                password: "hash",
-                email: "oriol@example.com",
-            }
-            await user.create(newUser);
-
-            assert.rejects(() => user.create(newUser), {
-                message: "duplicate key value violates unique constraint \"usuarios_nombre_key\""
+            dbCtrl.execute({
+                text: "DELETE FROM usuarios WHERE email = $1",
+                values: [fakeUser.email]
             });
         });
 
-        it("should return not null constraint violation", async function() {
-            let newUser = {
+        it("should throw error when creating two users with the same email", async function() {
+            const fakeUser = {
+                nombre: "FakeName",
+                password: "fakeHash",
+                email: "fake@example.com",
+            }
+            await user.create(fakeUser);
+            
+            const fakeUser2 = {
+                nombre: "FakeName2",
+                password: "fakeHash2",
+                email: fakeUser.email
+            };
+
+            await assert.rejects(() => user.create(fakeUser2), Error);
+
+            dbCtrl.execute({
+                text: "DELETE FROM usuarios WHERE email = $1",
+                values: [fakeUser.email]
+            });
+        });
+
+        it("should NOT insert a user without a password", async function() {
+            const newUser = {
                 nombre: "Oriol",
                 email: "oriol@example.com",
             }
 
-            assert.rejects(() => user.create(newUser), {
-                message: "null value in column \"password\" violates not-null constraint"
-            });
+            assert.rejects(() => user.create(newUser), Error);
+        });
+
+        it("should NOT insert a user without a name", async function() {
+            const newUser = {
+                email: "oriol@example.com",
+                password: "fakeHash"
+            };
+            assert.rejects(() => user.create(newUser), Error);
         });
     });
 });
