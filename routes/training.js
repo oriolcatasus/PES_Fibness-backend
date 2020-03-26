cb2abeb9c70574c457e93827846141962ba39d6f
oriol.catasus.i <oriol.catasus.i@est.fib.upc.edu>
2020-03-27 00:21:18 +0100

    Add logs for debugging purpouses

--- routes/training.js
+++ routes/training.js
@@ -5,22 +5,24 @@ const training = require("../src/models/trainingModel");
 const router = express.Router();
 
 router.delete('/:idElemento', async (req, res) => {
-    //console.log(req.body);
+    console.log(req.body);
     try {
         await training.del(req.params.idElemento);
         res.sendStatus(200);
     } catch (e) {
-        //console.error(e.message);
+        console.error(e.message);
         res.status(400).send(e.message);
     }
 });
 
 router.post('/', async (req, res) => {
+    console.log(req.params.id);
+    console.log(req.body);
     try {
         await training.create(req.body, req.params.id);
         res.sendStatus(200);
     } catch (e) {
-        //console.error(e.message);
+        console.error(e.message);
         res.status(400).send(e.message);
     }
 });
--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 
 router.post('/', async function(req, res, next) {
+    console.log(req.body);
     try {
         await user.create(req.body);
         res.sendStatus(201);
@@ -15,16 +16,18 @@ router.post('/', async function(req, res, next) {
 });
 
 router.delete('/:id', async (req, res) => {
+    console.log(req.params.id);
     try {
         await user.del(req.params.id);
         res.sendStatus(200);
     } catch (e) {
-        //console.error(e.message);
+        console.error(e.message);
         res.status(400).send(e.message);
     }
 });
 
 router.post('/validate', async function(req, res, next) {
+    console.log(req.body);
     try {
         const valid = await user.validate(req.body);
         res.send(valid);
