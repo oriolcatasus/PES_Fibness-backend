ed81f5f3220c45ea5b8857a691a4bd61078a638d
Oriol <oriol.catasus.i@est.fib.upc.edu>
2020-04-22 12:07:30 +0200

    Fix exercise migration

--- migrations/1587549837992_exercise.sql
+++ migrations/1587549837992_exercise.sql
@@ -1,3 +1,32 @@
 -- Up Migration
 
+drop table actividades cascade;
+drop table ejercicios;
+drop table deportes;
+
+
+create table actividades (
+	idActividad serial,
+	nombre varchar(50),
+	descripcion varchar(50),
+	tiempoEjecucion Integer,
+	idEntrenamiento int not null,
+	primary key (idActividad),
+	foreign key (idEntrenamiento) references entrenamientos (idElemento) on delete cascade
+);
+
+create table ejercicios (
+	idActividad int,
+	numSets Integer,
+	numRepeticiones Integer,
+	tiempoDescanso Integer,
+	primary key (idActividad),
+	foreign key (idActividad) references actividades (idActividad) on delete cascade
+);
+
+create table deportes(
+	idActividad int,
+	primary key (idActividad),
+	foreign key (idActividad) references actividades(idActividad) on delete cascade
+);
 -- Down Migration
