b6ad37e248e1284ca75ffbfe8d599b4fd06ef566
Kenny Alejandro <kennyangelsytem@gmail.com>
2020-04-23 11:06:33 +0200

    Little changes in the OpenAPI description of /trainig/{idTraining}/activities and change of id to idElemnt in training.js

--- src/routes/training.js
+++ src/routes/training.js
@@ -39,12 +39,13 @@ router.put('/:idElemento', async (req, res) => {
 });
 
 //Get the activities of a training
-router.get('/:id/activities', async function(req, res, next) {
+router.get('/:idElemento/activities', async function(req, res) {
     try {
-        const trainingSet = await training.activities(req.params.id);
-        res.send(trainingSet);
-    } catch (err) {
-        next(err);
+        const trainingSet = await training.activities(req.params.idElemento);
+        res.status(200).send(trainingSet);
+    } catch (e) {
+        console.error(e.message);
+        res.status(400).send(e.message);
     }
 })
 
