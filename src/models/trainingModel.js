cafb44690b65751b709b7f027fd3979f0cc8a965
Kenny Alejandro <kennyangelsytem@gmail.com>
2020-04-28 20:03:45 +0200

    solved errors with declaration of model training, just wrong word

--- src/models/trainingModel.js
+++ src/models/trainingModel.js
@@ -49,7 +49,7 @@ async function update(elemento, idElemento) {
 async function activities(idElemento) {
     
     let query = {
-        text: "SELECT a.idactividad AS idActividad, a.nombre, a.descripcion,a.tiempoejecucion AS tiempoEjecucion, e.numsets AS numSets, e.numrepeticiones AS numRepeticions, e.tiempodescanso AS tiempoDescanso\
+        text: "SELECT a.idactividad AS idActividad, a.nombre, a.descripcion,a.tiempoejecucion AS tiempoEjecucion, e.numsets AS numSets, e.numrepeticiones AS numRepeticiones, e.tiempodescanso AS tiempoDescanso\
                FROM actividades a inner join ejercicios e on a.idactividad = e.idactividad\
                WHERE a.idEntrenamiento = $1",
         values: [idElemento]
